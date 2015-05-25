f72e56a65087d4199e2dcde806b46c96d0ca323e
zppro <zpprozppro@gmail.com>
2015-05-25 11:32:09 +0800

    150525A

--- app.js
+++ app.js
@@ -2,24 +2,47 @@
  * Created by zppro on 15-5-4.
  */
 'use strict';
+
+var _ = require('underscore');
 var koa = require('koa');
-var router = require('koa-router');
+var router = require('koa-router')();
 var koaBody = require('koa-body')();
-var session = require('koa-session');
 var statelessauth = require('koa-statelessauth');
 var jwt = require('jsonwebtoken');
-var config = require('./config.js');
-var auth = require('./auth.js');
+var config = require('rfcore').config;
+var dataP = require('rfcore').dataP;
+var auth = require('./lib/auth.js');
+var bizGF001 = require('./biz/GF001.js');
 
 
-
-//console.log(config);
+//console.log(conf);
 
 var app = koa();
 
-// config
-config(app);
-console.dir(app.config);
+app.conf = {
+    biz:['GF001'],
+    auth: {
+        ignorePaths: ['/auth']
+    },
+    db: {
+        //mssql数据库配置
+        sqlserver: {
+            user: '数据库用户',
+            password: '密码',
+            server: '服务器IP',
+            port: '服务器端口',
+            database: '数据库名'
+        }
+    },
+    secure:{
+        authSecret:'认证密钥'
+    }
+};
+// conf
+config(app.conf,process.argv);
+
+
+
 // logger
 //app.use(function *(next){
 //    var start = new Date;
@@ -32,30 +55,59 @@ console.dir(app.config);
 //app.keys = ['leblue'];
 //app.use(session(app));
 
-// auth
-app.use(statelessauth({
-    validate: function (token) {
-        //This should go to a DB etc to get your user based on token
-        //token to user
-        try{
-            this.user = jwt.verify(token, app.config.secure.authSecret);
 
-        }catch(e){
-            this.status = 401;
-        }
 
-        return this.user;
-    }},
-    {
-        ignorePaths: ["/auth"]
+
+
+
+// dataP
+dataP.init('./dataP',{items:app.conf.biz.concat()}).then(function(){
+    // router
+    router
+        .get('/', function *(next) {
+            this.body = 'hello guest';
+            yield next;
+        })
+        .post('/auth', koaBody, auth(app));
+
+    //设置data service 远程
+    //var options = {getPath:'/data/get',setPath:'data/set',writePath:'data/write'};
+
+    dataP.remote.init(router);
+    app.conf.auth.ignorePaths.push(dataP.remote.settings.getPath);
+    app.conf.auth.ignorePaths.push(dataP.remote.settings.setPath);
+    app.conf.auth.ignorePaths.push(dataP.remote.settings.writePath);
+    //.post('/GF001', koaBody, bizGF001(app));
+    //require('./lib/koa-dataP.js')(router);
+
+    if(app.conf.biz.length > 0){
+        _.each(app.conf.biz,function(o){
+            router.post('/'+o, koaBody, require('./biz/'+o+'.js')(app,dataP));
+        });
     }
-));
+    app.use(router.routes())
+        .use(router.allowedMethods());
 
-app.use(router(app))
-    .get('/', function *(next) {
-        this.body = 'hello guest';
-    })
-    .post('/auth', koaBody, auth(app));
+    // auth
+    app.use(statelessauth({
+            validate: function (token) {
+                //This should go to a DB etc to get your user based on token
+                //token to user
+                try{
+                    this.user = jwt.verify(token, app.conf.secure.authSecret);
 
+                }catch(e){
+                    this.status = 401;
+                }
 
-app.listen(3000);
+                return this.user;
+            }},
+        {
+            ignorePaths: _.union(app.conf.auth.ignorePaths,_.map(app.conf.biz,function(o){ return '/'+o;}))
+        }
+    ));
+
+    // listen
+    app.listen(3000);
+});
+
---
+++ biz/GF001.js
@@ -0,0 +1,85 @@
+/**
+ * Created by zppro on 15-5-13.
+ */
+
+var exec = require('child-process-promise').exec;
+var log4js = require('log4js');
+
+var bizName='GF001';
+
+module.exports = function GF001(app,data){
+
+    //日志
+    log4js.configure({ appenders: [{
+        type: 'dateFile',
+        filename: 'logs/'+bizName,
+        pattern: '-yyyy-MM-dd.log',
+        alwaysIncludePattern: true,
+        category: bizName
+    }]});
+    var logger = log4js.getLogger(bizName);
+
+    //配置
+    var kCallins = 'callins',kCallouts = 'callouts',kCallees = 'callees';
+    var _callins = data.get(kCallins,bizName);
+    var _callouts = data.get(kCallouts,bizName);
+    var _callees =  data.get(kCallees,bizName);
+    var needSave = false;
+    if(!_callins){
+        _callins = ['18668001381'];
+        data.set(kCallins,_callins,bizName);
+        needSave = true;
+    }
+    if(!_callouts){
+        _callouts = ['13757140245','18668161133'];
+        data.set(kCallouts,_callouts,bizName);
+        needSave = true;
+    }
+    if(!_callees){
+        _callees = ['86723691'];
+        data.set(kCallees,_callees,bizName);
+        needSave = true;
+    }
+
+    if(needSave){
+        data.save(bizName).then(function(cont,success){
+            if(!success){
+                throw new Error('save '+bizName+'.json error');
+            }
+        });
+    }
+
+    return function * (next) {
+        ///body {"caller":"主叫号码","callee":"被叫号码"}
+        if(_callees.indexOf(this.request.body.callee) !== -1){
+            logger.info(JSON.stringify(this.request.body));
+            //检测主叫号码
+            if(_callins.indexOf(this.request.body.caller) === -1){
+                //非法的呼入码号
+                logger.error(JSON.stringify('非法的呼入码号'+this.request.body.caller));
+                this.body = '非法的呼入码号'+this.request.body.caller;
+                return;
+            }
+            var self = this;
+            var commandT = '/usr/local/atstar/voicestar/bin/fs_cli -x \'luarun '+bizName+'.lua [callNo] [wav] [times]\'';
+            _callouts.forEach(function(v){
+                var strCommand = commandT.replace('[callNo]',v).replace('[wav]','FT001-'+self.request.body.caller+'.wav').replace('[times]',2);
+                //console.log(strCommand);
+                exec(strCommand)
+                    .then(function(result){
+                        logger.info('请求外呼'+ v);
+                    }).fail(function (err) {
+                        logger.error(JSON.stringify(err));
+                    });
+            });
+
+            self.body = bizName;
+            return;
+        }
+        else{
+            this.body = 'invalid callee';
+        }
+
+        yield next;
+    };
+};
